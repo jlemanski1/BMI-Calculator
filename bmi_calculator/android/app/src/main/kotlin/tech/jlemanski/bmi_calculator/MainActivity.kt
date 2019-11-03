0b0255d16bb818f14d174dd7db21cee39a6d755a
Jonathan <jlemanski1@gmail.com>
2019-11-03 12:23:00 -0700

    Added base flutter project file with --org tech.jlemanski

---
+++ bmi_calculator/android/app/src/main/kotlin/tech/jlemanski/bmi_calculator/MainActivity.kt
@@ -0,0 +1,13 @@
+package tech.jlemanski.bmi_calculator
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
