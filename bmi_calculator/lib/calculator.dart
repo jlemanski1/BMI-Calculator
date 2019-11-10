d8c7de9c55dad311a9243d4c367645995a581827
Jonathan <jlemanski1@gmail.com>
2019-11-10 11:41:23 -0700

    Returns more verbose bmi results

--- bmi_calculator/lib/calculator.dart
+++ bmi_calculator/lib/calculator.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:flutter/widgets.dart';
+
 // Responsible for calculating the BMI of a user inputted height and weight
 class Calculator {
   final int height;
@@ -17,23 +19,39 @@ class Calculator {
 
   // Returns a result based on the BMI value calculated
   String getResult() {
-    if (_bmi >= 25) {
+    if (_bmi >= 40) {
+      return 'Extremely Obese';
+    } else if (_bmi >= 30) {
+      return 'Obese';
+    } else if (_bmi >= 25) {
       return 'Overweight';
-    } else if (_bmi > 18.5) {
-      return 'Normal';
+    } else if (_bmi >= 18.5) {
+      return 'Healthy Weight';
     } else {
       return 'Underweight';
     }
+    
   }
 
   // Returns an interpretation/advice based on the BMI value calculated
   String getInterpretation() {
-    if (_bmi >= 25) {
-      return 'You have a higher than normal body weight. Try to exercise more while watching your diet.';
-    } else if (_bmi > 18.5) {
-      return 'You have a normal body weight, keep it up!';
+    
+    if (_bmi >= 40) {
+      return 'You have are at an extremely high risk for type 2 diabetes, hypertension, and cardiovascular diseease. Go to your doctor for assistance.';
+    } else if (_bmi >= 30) {
+      return 'You have a high risk for type 2 diabetes, hypertension, and cardiovascular diseease. Exercise more and restrict your caloric intake.';
+    } else if (_bmi >= 25) {
+      return 'Exercise more and decrease your caloric intake to prevent an increased risk of type 2 diabetes, hypertension, and cardiovascular disease.';
+    } else if (_bmi >= 18.5) {
+      return "Keep up the active lifestyle, you're doing great!";
     } else {
-      return 'You have a lower than normal body weight. You can increase your caloric intake.';
+      return 'You should increase your caloric intake, and keep exercising to properly fuel your body,';
     }
+  }
+
+  // Returnsta colour for the result text based on the calculated bmi
+  Color getResultColour() {
+
+    return null;
   }
 }
