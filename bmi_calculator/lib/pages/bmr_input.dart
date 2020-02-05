f74a0335ce8072e2b6abfca289372b1f6a26490d
Jonathan <jlemanski1@gmail.com>
2020-02-05 15:06:59 -0700

    Updated Flutter, Started BMR input page design

--- bmi_calculator/lib/pages/bmr_input.dart
+++ bmi_calculator/lib/pages/bmr_input.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/components/measure.dart';
 import 'package:flutter/material.dart';
 
 class BMRInputPage extends StatefulWidget {
@@ -6,10 +7,33 @@ class BMRInputPage extends StatefulWidget {
 }
 
 class _BMRInputPageState extends State<BMRInputPage> {
+  Measurement selectedMeasure = Measurement.metric;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('BMR Input')
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'ezBMR',
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.lightGreen
+          ),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Row(
+            children: <Widget>[
+
+            ],
+          ),
+          ),
+        ],
+      ),
     );
   }
 }
